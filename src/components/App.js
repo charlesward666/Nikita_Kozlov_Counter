ecf6c6b9fb0a86cbd63930fb3735060e054aebd3
Charles Ward <charlesward2395@gmail.com>
2018-09-19 04:30:05 +0300

    comment

--- src/components/App.js
+++ src/components/App.js
@@ -1,72 +1,159 @@
-import React, {Component} from 'react'
-import {createStore} from 'redux'
+import React, { Component } from 'react';
 
-const initialState = { num: 0 }
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-function reducer(state = { num: 0 }, action) {
-	switch(action.type) {
-		case 'NEXT': return { num: state.num + action.amount }
-		case 'BACK': return { num: state.num - action.amount }
-		case 'RESET': return { num: 0 }
-		default: return state
-	}
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Input from '@material-ui/core/Input';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MenuItem from '@material-ui/core/MenuItem'
+
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
+});
+
+const fieldParams = ['Twin', 'Tripple', 'Quadro']
+
+
+
+class DialogSelect extends Component {
+
+  state = {
+    open: false,
+    items: [{
+      selectNumber: 0,
+      numberField: 0
+    }],
+    saveLetters: []
+  };
+
+
+  handleChangeItem = name => event => {
+    const items = [...this.state.items]
+    items[name]['selectNumber'] = event.target.value
+    this.setState({ items });
+  };
+
+  addField = () => {
+    const items = [...this.state.items]
+    items.push({selectNumber: 0, numberField: 0})
+    this.setState({
+      items: items
+    })
+  }
+
+  deleteField = (index) => {
+    const items = [...this.state.items]
+    items.splice(index, 1)
+    this.setState({
+      items: items
+    })
+  }
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  save = () => {
+    const items = [...this.state.items]
+    this.setState({
+      saveLetters: [...items],
+      items: [{selectNumber: 0, SelectField: 0}],
+      open: false
+    })
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <Button onClick={this.handleClickOpen}>Open select dialog</Button>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <DialogTitle>
+            Структура номеров
+            <Button onClick={this.handleClose}>Close</Button>
+          </DialogTitle>
+          <DialogContent>
+            <form className={classes.container} >
+            {this.state.items.map((item, index) => 
+              <FormControl className={classes.formControl} key={index}>
+                <Select
+                  name={`items.${index}.selectNumbe`}
+                  value={item.selectNumber}
+                  onChange={this.handleChangeItem(index)}
+                  input={<Input id={`some${index}`} name={`items.${index}.selectNumbe`} />}
+                >
+                  {fieldParams.map((itemParam, index) => 
+                      <MenuItem value={index} key={index}>{itemParam}</MenuItem>
+                  )}
+                </Select>
+
+                <TextField
+                  value={item.selectNumber}
+                />
+
+                <IconButton className={classes.button} aria-label="Delete" onClick={() => this.deleteField(index)}>
+                   <DeleteIcon />
+                </IconButton>
+              </FormControl>
+            )}
+
+              <Button type="button" onClick={this.addField}>Добавить</Button>
+
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.save} color="primary">
+              Сохранить
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Отмена
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <div>
+          <div><h1>Сохраненные письма</h1></div>
+          <div>
+            {this.state.saveLetters.map((item, index) =>
+              <div key={index}>{fieldParams[item.selectNumber]}</div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
+DialogSelect.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-function next(amount) {
-	return { type: 'NEXT', amount }
-}
-
-function back(amount) {
-	return { type: 'BACK', amount }
-}
-
-function reset() {
-	return { type: 'RESET' }
-}
-
-const store = createStore(reducer, initialState)
-
-class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.back = this.back.bind(this)
-		this.next = this.next.bind(this)
-		this.reset = this.reset.bind(this)
-	}
-
-	componentDidMount() {
-		store.subscribe(() => this.forceUpdate())
-	}
-
-	back () {
-		let amount = parseInt(this.refs.amount.value || 1)
-		store.dispatch(back(amount))
-	}
-
-	next () {
-		let amount = parseInt(this.refs.amount.value || 1)
-		store.dispatch(next(amount))
-	}
-
-	reset () {
-		store.dispatch(reset())
-	}
-	
-	render () {
-		const num = store.getState().num
-		return (
-			<div>
-				<button onClick={this.back}>-</button>
-				{num}
-				<button onClick={this.next}>+</button>
-				<button onClick={this.reset}>RESET</button>
-
-				<input type="text" ref="amount" defaultValue="1"></input>
-			</div>
-		)
-	}
-
-}
-
-export default App
+export default withStyles(styles)(DialogSelect);
